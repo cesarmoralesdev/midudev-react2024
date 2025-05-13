4c07515fb7cd0c33e7c64c23184f662da8dfd68c
Cesar Morales <cesarmorales@cardioperfusion.com>
2025-05-12 23:05:49 -0500

    Avance mouse-follower

---
+++ 03-mouse-follower/README.md
@@ -0,0 +1,11 @@
+
+Ver suscripciones al elemento window
+
+al ejecutar getEventListeners(window) en la consola del navegador
+
+Permite detectar suscripciones, si se estan suscribiendo algun evento constantemente puede provocar menor rendimiento de la aplicacion
+
+<StrictMode> hace que se ejecute 2 eveces el useffect, este bloque solo sirve para validar el montado de componentes, solo funciona en desarrollo en produccion lo ignora
+
+
+
---
+++ 03-mouse-follower/src/App.jsx
@@ -0,0 +1,71 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+
+function FollowMouse() {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    console.log("effect", { enabled });
+    const handleMove = (event) => {
+      const { clientX, clientY } = event;
+      console.log("handleMove", { clientX, clientY });
+      setPosition({ x: clientX, y: clientY });
+    };
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
+    }
+    // limpieza del efecto cuando se desmonte el componente
+    // cleanup
+    // se ejecuta cuando se desmonta el componente
+    // se ejecuta cuando cambian las dependencias
+    return () => {
+      console.log("cleanup");
+      window.removeEventListener("pointermove", handleMove);
+    };
+  }, [enabled]);
+  return (
+    <>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "rgba(0,0,0,0.5)",
+          border: "1px solid #fff",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      ></div>
+      <h1>Proyecto 3</h1>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? "Desactivar" : "Activar"} seguir puntero
+      </button>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <main>
+      <FollowMouse />
+    </main>
+  );
+
+  // const [mounted, setMounted] = useState(true);
+
+  // return (
+  //   <main>
+  //     { mounted && <FollowMouse /> }
+  //     <button onClick={() => setMounted(!mounted)}>
+  //       {mounted ? "Desmontar" : "Montar"} FollowMouse
+  //     </button>
+  //   </main>
+  // );
+}
+
+export default App;
